2d730ed18219c6bb03f4be2f07c3ebf76d9dc777
Jan <jan.honsbrok@gmail.com>
2019-04-19 17:32:21 +0200

    added io library to handle point cloud loading and command line executable

---
+++ src/io/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Jan on 19.04.2019.
+//
+#include <iostream>
+#include <Greeter.h>
+
+int main(){
+  Greeter greeter;
+  std::cout << greeter.getGreeting() << std::endl;
+  return 0;
+}
