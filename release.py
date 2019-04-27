f163cc7534eacf305240b5030e46596261669ab5
Jan <jan.honsbrok@gmail.com>
2019-04-27 18:19:34 +0200

    adjusted release script

--- release.py
+++ release.py
@@ -1,7 +1,11 @@
-import subprocess
-import github_release
-from dotenv import load_dotenv
 import ConfigParser
+import glob
+import os
+import shutil
+import subprocess
+
+from dotenv import load_dotenv
+
 
 def read_current_version():
     config = ConfigParser.ConfigParser()
@@ -13,24 +17,44 @@ def should_release():
     current_version = read_current_version()
     current_tag = get_current_tag()
     if current_tag:
-        if current_tag == "v"+current_version:
+        if current_tag == "v" + current_version:
             return True
         else:
-            print "current tag and current version have to match: current tag: {}, current version: {}".format(current_tag, current_version)
+            print 'current tag and current version have to match: current tag: "{}", current version: "v{}"'.format(
+                current_tag, current_version)
             return False
 
 
 def get_current_tag():
     # return subprocess.check_output("git tag -l --points-at HEAD")
-    return subprocess.check_output("git describe --tags")
+    return subprocess.check_output("git describe --tags").replace("\n", "")
 
 
 def collect_files():
-    return ['build/src/Release/claudius-*.zip']
+    return ['build/src/plugin/Release/claudius-*.zip']
 
 
 def read_credentials():
     load_dotenv()
+
+
+def deploy_to_one_drive(files, release=False):
+    if release:
+        print "doing release"
+    else:
+        print "deploy dev build"
+    matched_files = set()
+    for file_pattern in files:
+        for f in glob.glob(file_pattern):
+            matched_files.add(f)
+
+    for matched_file in matched_files:
+        target = os.path.join(r"C:\release_share\claudius_builds",
+                              'releases' if release else 'dev_builds',
+                              os.path.basename(matched_file))
+
+        print "copy {} => {}".format(matched_file, target)
+        shutil.copy(matched_file, target)
 
 
 def upload_release():
@@ -38,13 +62,10 @@ def upload_release():
     current_tag = get_current_tag()
     files = collect_files()
 
-    github_release.gh_release_create("Latios96/claudius", current_tag, name=current_tag, asset_pattern=files)
-    github_release.gh_asset_upload("Latios96/claudius", current_tag, asset_pattern=files)
+    # github_release.gh_release_create("Latios96/claudius", current_tag, name=current_tag, asset_pattern=files)
+    # github_release.gh_asset_upload("Latios96/claudius", current_tag, asset_pattern=files)
 
 
 if __name__ == '__main__':
-    if should_release():
-        print "we do upload a release"
-        upload_release()
-    else:
-        print "skipping release upload"
+    files = collect_files()
+    deploy_to_one_drive(files, release=should_release())
