c745da3f0ba289f8875c951a4d1150a4aee2e22a
Jan <jan.honsbrok@gmail.com>
2019-05-22 21:36:33 +0200

    second try

--- src/io/io_lib/PtsParticleReader.cpp
+++ src/io/io_lib/PtsParticleReader.cpp
@@ -90,10 +90,28 @@ const unsigned int PTS_Z_INDEX = 1;
 
 const unsigned int CHUNK_SIZE = 100000000; // 100 MB
 
-bool readChunk(std::istream &file, std::vector<char> buffer){
-    file.read(buffer.data(), CHUNK_SIZE);
-    bool reachedEndOfFile = file.gcount() < CHUNK_SIZE; // todo respect that we could end in the middle of a line
-    return reachedEndOfFile;
+struct ReadResult{
+  ReadResult(unsigned int count, bool reachedEndOfFile) : count(count), reachedEndOfFile(reachedEndOfFile) {}
+  unsigned int count;
+  bool reachedEndOfFile;
+};
+ReadResult readChunk(std::istream &file, std::vector<char> &buffer, unsigned int totalSize){
+	unsigned int oldPos = file.tellg();
+	unsigned int countToRead = totalSize - oldPos;
+	if(buffer.size() < countToRead){
+        file.read(buffer.data(), buffer.size());
+	}
+	else{
+        file.read(buffer.data(), countToRead);
+	};
+    std::streamsize read_count = file.gcount();
+    bool reachedEndOfFile = read_count < CHUNK_SIZE; // todo respect that we could end in the middle of a line
+    unsigned int i = read_count - 1;
+    while(buffer[i] != '\n' && i > 0){
+        i--;
+    }
+
+    return {i+1, reachedEndOfFile};
 }
 
 void PtsParticleReader::readParticles(std::istream &file, ParticleContainer &particleContainer) {
@@ -151,14 +169,32 @@ void PtsParticleReader::readParticles(std::istream &file, ParticleContainer &par
 		bufferString.clear();
         bufferOperator.toFirstCharAfterNewLine();
     }*/
+    /*file.seekg(0, std::ios::end);
+    unsigned int totalSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+	unsigned int oldPos = file.tellg();
+	file.get();
+	file.unget();
+	unsigned int oldPos_ = file.tellg();
     std::vector<char> str;
     str.resize(CHUNK_SIZE);
-    bool reachedEndOfFile = readChunk(file, str);
+    ReadResult readResult = readChunk(file, str, totalSize);
     processChunk(str, particleContainer);
-    while(!reachedEndOfFile){
-        reachedEndOfFile = readChunk(file, str);
+    int chunkCount = 1;
+    std::cout << "Processed chunk " << chunkCount << std::endl;
+    while(!readResult.reachedEndOfFile){
+        readResult = readChunk(file, str, totalSize);
+        str.resize(readResult.count);
         processChunk(str, particleContainer);
-    }
+        std::cout << "Processed chunk " << chunkCount << std::endl;
+    }*/
+	std::vector<char> buffer(1024, 0); //reads only the first 1024 bytes
+
+	while (file.read(buffer.data(), buffer.size())) {
+		std::streamsize s = file.gcount();
+		///do with buffer
+		buffer.size();
+	}
 
 }
 std::string PtsParticleReader::getReaderName() {
@@ -170,6 +206,11 @@ void PtsParticleReader::processChunk(std::vector<char> &str, ParticleContainer &
 
     CharBufferOperator determineData(str);
     // skip particle count information
+    const bool isFirstChunk = particleContainer.particleCount() == 0;
+    if(isFirstChunk){
+        determineData.readUntilNewLine(bufferString);
+        int particleCount = std::stoi(bufferString);
+    }
     determineData.toFirstCharAfterNewLine();
     // read first line and check how many values we have
     determineData.readUntilNewLine(bufferString);
@@ -184,6 +225,10 @@ void PtsParticleReader::processChunk(std::vector<char> &str, ParticleContainer &
     CharBufferOperator bufferOperator(str);
     bufferOperator.toFirstCharAfterNewLine();
     bufferString.clear();
+
+    if(isFirstChunk){
+        // todo reserve data
+    }
 
     while(bufferOperator.canContinue()){
         bufferOperator.readUntilNewLine(bufferString);
