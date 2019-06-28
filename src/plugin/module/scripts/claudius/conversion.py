0570044277eb9ac4906b738ec42cc98870714529
Jan <jan.honsbrok@gmail.com>
2019-06-28 09:24:50 +0200

    conversion script: automatic import of .ass files

--- src/plugin/module/scripts/claudius/conversion.py
+++ src/plugin/module/scripts/claudius/conversion.py
@@ -32,17 +32,24 @@ def export_node_to_ass(claudius_node, target_path):
     pm.select(old_sel)
 
 
-def chunks_to_ass(file_counter, output_template):
+def chunks_to_ass(chunk_files):
     claudius_node = pm.createNode('ClaudiusVisualizer')
     claudius_node.renderWithColor.set(True)
 
-    for i, chunk_file in enumerate(glob.glob(output_template.format('*'))):
+    ass_files = []
+
+    for i, chunk_file in enumerate(chunk_files):
         print "Converting chunk {}".format(i)
         claudius_node.particleFile.set(chunk_file)
         target_path = os.path.splitext(chunk_file)[0] + ".ass"
         export_node_to_ass(claudius_node, target_path)
+        ass_files.append(target_path)
 
-    pm.delete(claudius_node)
+    # NOTE: ClaudiusVisualizer always has a transform as a parent. If we only delete
+    # the ClaudiusVisualizer node, we have an unused transform in our scene.
+    pm.delete(claudius_node.parent(0))
+
+    return ass_files
 
 
 def process_file(input_path, output_template):
@@ -52,12 +59,17 @@ def process_file(input_path, output_template):
     lines = []
     start = time.time()
     first_line = True
+
+    written_files = []
+
     with open(input_path, 'r') as f:
         for line in f:
             if first_line:
                 first_line = False
                 continue
             if counter == MAX:
+                file_name = output_template.format(file_counter)
+                written_files.append((file_name))
                 with open(output_template.format(file_counter), "w") as chunk_file:
                     print "writing chunk {}".format(file_counter)
                     chunk_file.write(str(len(lines)) + "\n")
@@ -68,7 +80,9 @@ def process_file(input_path, output_template):
             lines.append(line)
             counter += 1
 
-    with open(output_template.format(file_counter), "w") as chunk_file:
+    file_name = output_template.format(file_counter)
+    written_files.append((file_name))
+    with open(file_name, "w") as chunk_file:
         print "writing chunk {}".format(file_counter)
         chunk_file.write(str(len(lines)) + "\n")
         chunk_file.writelines(lines)
@@ -76,14 +90,22 @@ def process_file(input_path, output_template):
     print "Time to split pts into chunks: {}".format(pretty_time(time.time() - start))
 
     ass_start = time.time()
-    chunks_to_ass(file_counter, output_template)
+    ass_files = chunks_to_ass(written_files)
 
     print "Time to create .ass files {}".format(pretty_time(ass_start - start))
     print "Total time: {}".format(pretty_time(time.time() - start))
+    return ass_files
+
+
+def import_ass_files(ass_files):
+    for i, ass_files in enumerate(ass_files):
+        print "Importing chunk {}".format(i)
+        cmds.file(ass_files, i=True)
 
 
 def do_convert():
-    input_path = QtWidgets.QFileDialog.getOpenFileName()[0]
+    input_path = QtWidgets.QFileDialog.getOpenFileName()[0] # todo check for .pts files
     if input_path:
         output_template = create_output_template(os.path.dirname(input_path), input_path)
-        process_file(input_path, output_template)
+        ass_files = process_file(input_path, output_template)
+        import_ass_files(ass_files)
